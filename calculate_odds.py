120b12dada89f7003f562166041754785b2d367a
Vivek.gogtay <vivek.gogtay@MacBook12.local>
2023-07-01 17:01:11 +0100

    start

---
+++ calculate_odds.py
@@ -0,0 +1,10 @@
+from card_file import Card, ALLOWED_CARDS
+
+def calculate(main_player,drawn_cards,number_of_opponents,number_of_sims):
+
+    temp_allowed_cards = ALLOWED_CARDS
+    for card_obj in main_player+drawn_cards:
+        temp_allowed_cards -= zip(card_obj.number,card_obj.suit)
+    
+    for n in range(number_of_sims):
+        for 
---
+++ main.py
@@ -0,0 +1,7 @@
+from card_file import Card
+
+main_player = [Card(7,'H'),Card(2,'C')]
+drawn_cards = [Card(10,'S'), Card(9,'S'), Card (5,'H')]
+
+number_of_opponents = 2
+
