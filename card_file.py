e5d9574c6a882404333e0e1b561912dce1474f7a
Vivek.gogtay <vivek.gogtay@MacBook12.local>
2023-07-02 21:03:29 +0100

    nothing really

--- card_file.py
+++ card_file.py
@@ -2,13 +2,14 @@ from itertools import product
 ALLOWED_SUITS = ['H','D','C','S']
 ALLOWED_NUMBERS = [i for i in range(1,14)]
 ALLOWED_CARDS = list(product(ALLOWED_NUMBERS,ALLOWED_SUITS))
+
 class Card:
 
-    
     def __init__(self,number,suit) -> None:
         self.number = number
         self.suit = suit
         
+class Player(Card):
+    pass
     
-
     
