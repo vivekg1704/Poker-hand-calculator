cd8a517bc8ef01f0011d34e3d81403d3f2fe9808
Vivek.gogtay <vivek.gogtay@MacBook12.local>
2023-07-03 02:58:29 +0100

    added some functions and started defining hands

--- card_file.py
+++ card_file.py
@@ -1,7 +1,4 @@
 from itertools import product
-ALLOWED_SUITS = ['H','D','C','S']
-ALLOWED_NUMBERS = [i for i in range(1,14)]
-ALLOWED_CARDS = list(product(ALLOWED_NUMBERS,ALLOWED_SUITS))
 
 class Card:
 
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from card_file import Card
+import numpy as np
+from evaluate_hand import hands
 
 main_player = [Card(7,'H'),Card(2,'C')]
 drawn_cards = [Card(10,'S'), Card(9,'S'), Card (5,'H')]
 
 number_of_opponents = 2
+
+cards = np.array([Card(8,'C'), Card(11,'H'),Card(12,'H'),Card(10,'H'),Card(9,'H')])
+
+print(hands(cards))
+
+
 
